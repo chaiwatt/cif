6410461dcbee39924a6bdfca03e98769a19fc994
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2023-09-29 08:49:40 +0700

    document system update

--- resources/views/groups/dashboard/document-system.blade.php
+++ resources/views/groups/dashboard/document-system.blade.php
@@ -20,7 +20,7 @@
                 <div class="info-box">
                     <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
                     <div class="info-box-content">
-                        <span class="info-box-text">Messages</span>
+                        <span class="info-box-text">ลาป่วย</span>
                         <span class="info-box-number">1,410</span>
                     </div>
                 </div>
@@ -30,7 +30,7 @@
                 <div class="info-box">
                     <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
                     <div class="info-box-content">
-                        <span class="info-box-text">Bookmarks</span>
+                        <span class="info-box-text">พักร้อน</span>
                         <span class="info-box-number">410</span>
                     </div>
                 </div>
@@ -40,7 +40,7 @@
                 <div class="info-box">
                     <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
                     <div class="info-box-content">
-                        <span class="info-box-text">Uploads</span>
+                        <span class="info-box-text">ลากิจ</span>
                         <span class="info-box-number">13,648</span>
                     </div>
                 </div>
@@ -50,7 +50,7 @@
                 <div class="info-box">
                     <span class="info-box-icon bg-danger"><i class="far fa-star"></i></span>
                     <div class="info-box-content">
-                        <span class="info-box-text">Likes</span>
+                        <span class="info-box-text">ลาอื่น ๆ</span>
                         <span class="info-box-number">93,139</span>
                     </div>
                 </div>
