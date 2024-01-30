ca0881a0ed5eb433fffb8a5ce1465a28a7de4306
MrChim's <mrchimky@gmail.com>
2024-01-30 23:25:45 +0700

    Update view.blade.php

--- resources/views/groups/learning-system/learning/learning-list/view.blade.php
+++ resources/views/groups/learning-system/learning/learning-list/view.blade.php
@@ -36,7 +36,7 @@
                                 <div class="accordion-item">
                                   <h2 class="accordion-header">
                                     <button class="accordion-button {{ $key == 0 ? "": "collapsed" }}" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-{{ $key }}" aria-expanded="{{ $key == 0 ? "true": "false" }}" aria-controls="collapse-{{ $key }}">
-                                        <h6>{{$chapter->name}}</h6>
+                                        <h6 class="m-0">{{$chapter->name}}</h6>
                                     </button>
                                   </h2>
                                   <div id="collapse-{{ $key }}" class="accordion-collapse collapse {{ $key == 0 ? "show": "" }}">
